9bba804f788b66f7bfe8f6b1312b06096a036626
kentozaka46 <kentoball52@gmail.com>
2022-01-26 20:32:23 +0900

    jsファイル作成

---
+++ src/main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+var app1 = new Vue({
+    el: '#app1',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
