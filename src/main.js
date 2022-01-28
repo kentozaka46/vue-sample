c22202b3146cad3c4a12789a2a88412a5ead765e
kentozaka46 <kentoball52@gmail.com>
2022-01-28 21:25:35 +0900

    郵便番号のサンプル作成

--- src/main.js
+++ src/main.js
@@ -1,51 +1,32 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
-    }
-})
+const URL_API = "https://api.zipaddress.net/";
 
-var app2 = new Vue({
-    el: '#app-2',
+const vm = new Vue({
+    el: '#demo',
     data: {
-        message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-})
-
-var app3 = new Vue({
-    el: '#app-3',
-    data: {
-        seen: true
-    }
-})
-
-var app4 = new Vue({
-    el: '#app-4',
-    data: {
-        todos: [
-            { text: 'Learn JavaScript' },
-            { text: 'Learn Vue' },
-            { text: 'Build something awesome' }
-        ]
-    }
-})
-
-var app5 = new Vue({
-    el: '#app-5',
-    data: {
-        message: 'Hello Vue.js!'
+        inputZip: '',
+        defaultZip: '1000000',
+        results: '結果がここに表示されます'
+    },
+    computed: {
+        computedZip: function() {
+            return !isNaN(this.inputZip) && this.inputZip.length == 7 ? this.inputZip : this.defaultZip
+        }
     },
     methods: {
-        reverseMessage: function() {
-            this.message = this.message.split('').reverse().join('')
+        getAddress: function(z) {
+            let params = { params: { zipcode: z } };
+            axios
+                .get(URL_API, params)
+                .then(res => {
+                    this.results = res.data.code == 200 ? res.data.data.fullAddress : res.data.message;
+                });
         }
-    }
-})
-
-var app6 = new Vue({
-    el: '#app-6',
-    data: {
-        message: 'Hello Vue!'
+    },
+    filters: {
+        filterZip: function(d) {
+            let buf = ('0000000' + d).slice(-7);
+            return isNaN(buf) ? '1000000' : buf
+        }
     }
 })
 
