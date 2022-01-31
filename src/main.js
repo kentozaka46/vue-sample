1570eb8031f7c99713e5107503baa5eb98bcae39
kentozaka46 <kentoball52@gmail.com>
2022-01-31 22:12:06 +0900

    コメント修正

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ const vm = new Vue({
         results: '結果がここに表示されます'
     },
     computed: {
+        // 入力されたものが数字かどうか、7文字かどうかで処理を分岐
         computedZip: function() {
             return !isNaN(this.inputZip) && this.inputZip.length == 7 ? this.inputZip : this.defaultZip
         }
@@ -15,6 +16,7 @@ const vm = new Vue({
     methods: {
         getAddress: function(z) {
             let params = { params: { zipcode: z } };
+            // 郵便番号検索のAPIからデータを取得する処理
             axios
                 .get(URL_API, params)
                 .then(res => {
@@ -23,6 +25,7 @@ const vm = new Vue({
         }
     },
     filters: {
+        // 7文字以上の数字が入力された際に、7文字に矯正する処理
         filterZip: function(d) {
             let buf = ('0000000' + d).slice(-7);
             return isNaN(buf) ? '1000000' : buf
